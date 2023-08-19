a9499f31bf10411a3cc5ed848a35ba789bd20f8a
Victor Wildner <vwildner@redhat.com>
2023-08-19 15:49:10 -0300

    fix: strip argc and argv from main

--- ex00/main.c
+++ ex00/main.c
@@ -15,13 +15,8 @@ void	rush(int x, int y);
 	Esse é o programa principal, ele é responsável apenas por chamar a função
 	`rush` com os argumentos passados pelo usuário.
 */
-int	main(int argc, char *argv[])
+int	main()
 {
-	int	x;
-	int	y;
-
-	x = argv[1][0] - '0';
-	y = argv[2][0] - '0';
-	rush(x, y);
+	rush(5, 3);
 	return (0);
 }
