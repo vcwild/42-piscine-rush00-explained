2457d3117619de24ddab4f40cc6c212ea2496933
Victor Wildner <vwildner@redhat.com>
2023-08-19 15:48:31 -0300

    docs: add comment about prototype declaration

--- ex00/main.c
+++ ex00/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <limits.h>
 
+/*
+	É necessário declarar a função `rush` antes de usá-la porque ela só é
+	implementada mais depois no arquivo `rush02.c`, porém o compilador ainda
+	não sabe disso e vai dar erro se não declararmos a função antes.
+	Precisamos sempre avisar o compilador sobre as funções que vamos usar.
+	Essa declaração aqui nós costumeiramente chamamos de "protótipo" da função,
+	porque ela é uma prévia da função que vamos usar, mas não é a função em si.
+*/
 void	rush(int x, int y);
 
 /*
